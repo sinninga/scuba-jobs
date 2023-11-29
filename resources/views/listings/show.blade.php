be6e581f99147eafb4f23f943f73f3207bda53cc
Adam Sinning <asinning25@gmail.com>
2023-11-29 12:37:14 -0600

    layout made into component

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,6 +1,5 @@
-@extends('layout')
+<x-layout>
 
-@section('content')
 @include('partials._search')
 
 <a href="/" class="inline-block text-black ml-4 mb-4"
@@ -50,4 +49,4 @@
     </x-card>
 </div>
 
-@endsection
+</x-layout>
