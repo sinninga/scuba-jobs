8e0a063dcd5d8042df0dc5edffe6adc21cba497f
Adam Sinning <asinning25@gmail.com>
2023-11-28 23:49:37 -0600

    db seeded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
         Listing::create([
             'title' => 'Divemaster',
@@ -26,6 +26,17 @@ class DatabaseSeeder extends Seeder
             to instruct classes for PADI open water students and
             to guide groups of certified divers in sites in Monad
             Shoal and Gato Island'
+        ]);
+
+        Listing::create([
+            'title' => 'Dive Instructor',
+            'tags' => 'divemaster, instructor',
+            'company' => 'Blue Hole Safaris',
+            'location' => 'Dahab, Egypt',
+            'email' => 'blueholesafaris@email.com',
+            'website' => 'www.blueholesafaris.com',
+            'description' => 'This is a position for an instructor
+            to teach groups of divers in sites in Bells/Blue Hole'
         ]);
     }
         // \App\Models\User::factory()->create([
