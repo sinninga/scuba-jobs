7f674ea62b834125d124d0da95730a70113ccea3
Adam Sinning <asinning25@gmail.com>
2023-11-29 11:36:14 -0600

    search bar added

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 
+@include('partials._hero')
+@include('partials._search')
+
 @unless(count($listings) == 0)
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
@@ -15,7 +18,7 @@
             />
             <div>
                 <h3 class="text-2xl">
-                    <a href="show.html">{{$listing->title}}</a>
+                    <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
                 </h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                 <ul class="flex">
