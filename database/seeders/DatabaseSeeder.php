212df56627d2a747cb5795273254c3e2a6d5145d
Adam Sinning <asinning25@gmail.com>
2023-11-28 23:45:59 -0600

    listing model imported

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,11 +11,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
         \App\Models\User::factory(10)->create();
 
-        Listing::create(
+        Listing::create([
             'title' => 'Divemaster',
             'tags' => 'divemaster, instructor',
             'company' => 'Thresher Divers',
@@ -24,13 +25,12 @@ class DatabaseSeeder extends Seeder
             'description' => 'This is a position for a divemaster
             to instruct classes for PADI open water students and
             to guide groups of certified divers in sites in Monad
-            Shoal and Gato Island',
-
-        )
-
+            Shoal and Gato Island'
+        ]);
+    }
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
     }
-}
+
