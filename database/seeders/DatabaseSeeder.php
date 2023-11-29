212d4fbb4911cdfdad3eeacd26167119fff936c0
Adam Sinning <asinning25@gmail.com>
2023-11-29 00:00:01 -0600

    database seeder update for Listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,29 +15,31 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
-        Listing::create([
-            'title' => 'Divemaster',
-            'tags' => 'divemaster, instructor',
-            'company' => 'Thresher Divers',
-            'location' => 'Malapascua, Phillipines',
-            'email' => 'thresherdivers@email.com',
-            'website' => 'www.thresherdivers.com',
-            'description' => 'This is a position for a divemaster
-            to instruct classes for PADI open water students and
-            to guide groups of certified divers in sites in Monad
-            Shoal and Gato Island'
-        ]);
+        Listing::factory(5)->create();
 
-        Listing::create([
-            'title' => 'Dive Instructor',
-            'tags' => 'divemaster, instructor',
-            'company' => 'Blue Hole Safaris',
-            'location' => 'Dahab, Egypt',
-            'email' => 'blueholesafaris@email.com',
-            'website' => 'www.blueholesafaris.com',
-            'description' => 'This is a position for an instructor
-            to teach groups of divers in sites in Bells/Blue Hole'
-        ]);
+        // Listing::create([
+        //     'title' => 'Divemaster',
+        //     'tags' => 'divemaster, instructor',
+        //     'company' => 'Thresher Divers',
+        //     'location' => 'Malapascua, Phillipines',
+        //     'email' => 'thresherdivers@email.com',
+        //     'website' => 'www.thresherdivers.com',
+        //     'description' => 'This is a position for a divemaster
+        //     to instruct classes for PADI open water students and
+        //     to guide groups of certified divers in sites in Monad
+        //     Shoal and Gato Island'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Dive Instructor',
+        //     'tags' => 'divemaster, instructor',
+        //     'company' => 'Blue Hole Safaris',
+        //     'location' => 'Dahab, Egypt',
+        //     'email' => 'blueholesafaris@email.com',
+        //     'website' => 'www.blueholesafaris.com',
+        //     'description' => 'This is a position for an instructor
+        //     to teach groups of divers in sites in Bells/Blue Hole'
+        // ]);
     }
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
