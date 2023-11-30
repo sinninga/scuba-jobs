58a1a3703f9f6a4a45e5d95608d2660b1f367e82
Adam Sinning <asinning25@gmail.com>
2023-11-30 14:14:06 -0600

    register view added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,7 +25,7 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>ScubaJobs | Find Scuba Jobs & Internships</title>
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
@@ -34,12 +34,12 @@
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
-                    <a href="register.html" class="hover:text-laravel"
+                    <a href="/register" class="hover:text-laravel"
                         ><i class="fa-solid fa-user-plus"></i> Register</a
                     >
                 </li>
                 <li>
-                    <a href="login.html" class="hover:text-laravel"
+                    <a href="/login" class="hover:text-laravel"
                         ><i class="fa-solid fa-arrow-right-to-bracket"></i>
                         Login</a
                     >
@@ -52,7 +52,7 @@
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
         >
-        <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+        <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
         <a
             href="/listings/create"
             class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
