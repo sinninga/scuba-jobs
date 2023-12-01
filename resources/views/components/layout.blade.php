5864c07e7c74c0361542ff0b707f210866b6af89
Adam Sinning <asinning25@gmail.com>
2023-12-01 17:43:32 -0600

    hero color scheme update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,7 +30,7 @@
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
             <a href="/"
-                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
+                ><img class="w-24" src="{{asset('images/manta.jpeg')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 @auth
@@ -72,7 +72,7 @@
         {{$slot}}
     </main>
     <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-sky-500 text-white h-24 mt-24 opacity-90 md:justify-center"
         >
         <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
         <a
--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,5 +1,5 @@
 <section
-class="relative h-72 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4">
+class="relative h-72 bg-sky-500 flex flex-col justify-center align-center text-center space-y-4 mb-4">
     <div
         class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
         style="background-image: url('images/laravel-logo.png')"
@@ -7,10 +7,10 @@ class="relative h-72 bg-laravel flex flex-col justify-center align-center text-c
 
     <div class="z-10">
         <h1 class="text-6xl font-bold uppercase text-white">
-            Lara<span class="text-black">Gigs</span>
+            Scuba<span class="text-black">Jobs</span>
         </h1>
         <p class="text-2xl text-gray-200 font-bold my-4">
-            Find or post Laravel jobs & projects
+            Find or post Diving jobs & internships
         </p>
         <div>
             <a
