f8d4130c8ab72e9e5c9f25fab2372ca1305d357d
Adam Sinning <asinning25@gmail.com>
2024-01-05 20:04:27 -0600

    footer date update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -74,7 +74,7 @@
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-sky-500 text-white h-24 mt-24 opacity-90 md:justify-center"
         >
-        <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
+        <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
         <a
             href="/listings/create"
             class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
